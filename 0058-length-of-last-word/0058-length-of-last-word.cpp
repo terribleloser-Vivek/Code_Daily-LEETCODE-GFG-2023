d81cfa2c1459a2b2da766a7e43a5b8bd4ef24ed3
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-02-16 23:02:36 +0530

    Time: 4 ms (36.89%), Space: 6.6 MB (56.61%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0, i=s.size()-1;
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            i--; ans++;
+        }
+        return ans;
+    
+    }
+};
