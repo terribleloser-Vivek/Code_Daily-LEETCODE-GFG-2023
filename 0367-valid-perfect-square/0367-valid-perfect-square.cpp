fbfe520a8ffea99ba891a25e5357cd24d409dc3a
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-03-23 22:52:41 +0530

    Time: 3 ms (47.61%), Space: 6 MB (50.31%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+
+    if(num==1||num==4)
+        return true;
+    else
+    {
+        int start=0;
+        int end=num;
+        while(start<=end)
+        {
+            long long int mid=start+(end-start)/2;
+            if(mid*mid==num)
+                return true;
+            else if(mid*mid>num)
+                end=mid-1;
+            else if(mid*mid<num)
+                start=mid+1;
+        }
+    }
+    return false;
+
+    }
+};
