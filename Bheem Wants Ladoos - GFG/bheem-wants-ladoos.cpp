3175c142fe4b81ddd7319c6eefbd76e34b149804
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-04-20 22:28:00 +0530

    Added solution - LeetHub

---
+++ Bheem Wants Ladoos - GFG/bheem-wants-ladoos.cpp
@@ -0,0 +1,177 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+// node structure:
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+class Solution{
+
+    public:
+    unordered_map<int, vector<int>> adj;
+    unordered_map<int, bool> vis;
+    void filladj(Node* root){
+        if(root==NULL) return;
+        if(root->left){
+            adj[root->data].push_back(root->left->data);
+            adj[root->left->data].push_back(root->data);
+        }
+        if(root->right){
+            adj[root->data].push_back(root->right->data);
+            adj[root->right->data].push_back(root->data);
+        }
+        filladj(root->left);
+        filladj(root->right);
+            
+    }
+    int ladoos(Node* root, int home, int k)
+    {
+        // Your code goes here
+        filladj(root);
+        int ans=0;
+        for(auto it:adj){
+            if(it.first==home){
+                ans=home;
+                break;
+            }
+        }
+        if(ans!=home) return home;
+        queue<int> q;
+        q.push(ans);
+        vis[ans]=true;
+        while(!q.empty() && k--){
+            int size=q.size();
+            while(size--){
+                int temp=q.front();
+                q.pop();
+                for(int i:adj[temp]){
+                    if(!vis[i]){
+                        ans+=i;
+                        q.push(i);
+                        vis[i]=true;
+                    }
+                    
+                }
+            }
+        }
+        return ans;
+    }
+
+
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        
+        int home,k;
+        cin>> home >> k;
+        getchar();
+        Solution obj;
+        cout<< obj.ladoos(root,home,k) << endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
