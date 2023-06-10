b49a5956b00ded3e179b3e5cd790b46c6f35ab9d
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-06-10 16:41:32 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+           vector<int>neg,pos;
+            for(int i=0;i<n;i++){
+                if(arr[i]<0)
+                    neg.push_back(arr[i]);
+                else
+                    pos.push_back(arr[i]);
+            }
+            for(int i=0;i<n;i++){
+                if(i<neg.size()){
+                    arr[i]=neg[i];
+                }
+                else{
+                    arr[i]=pos[i-neg.size()];
+                }
+            }
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
