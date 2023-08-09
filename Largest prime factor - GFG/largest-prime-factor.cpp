e738decbb4ec282a1e4289169a043b2b1996eea3
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-08-10 00:40:21 +0530

    Added solution - LeetHub

--- Largest prime factor - GFG/largest-prime-factor.cpp
+++ Largest prime factor - GFG/largest-prime-factor.cpp
@@ -6,19 +6,41 @@ using namespace std;
 class Solution{
 public: 
     long long int largestPrimeFactor(int N){
-       long long ans=1;
+        /*N --- 5 / 24 -2*2*2*3
+      ans --- 1
+      24 - 2 *  3   4
+         - 12   4   4
+      
+      loop 1-N
+       1 --- 5%1 == 0
+       2 --- 5%2 != 0 --  NF
+       3 --- 5%3 !=0 --- NF
+       4 --- 5%4 !=0 --- NF
+       5 --- 5%5==0 -- F
+       
+       array/vect --- > 1,5 
+       vect max -- > 5
+       5---> ans */
+       
+        long long int ans;
         int m=N;
+        vector<int> an,prime;
+        an.push_back(1);
         for(int i=2;i*i<=m;i++)
         {
-            while(N%i==0)
-            {
-                ans=i;
-                N=N/i;
-            }
-        }
-        // ans=max(ans,N);
-        ans=(ans>N)?ans:N;
-        return ans;
+            while(N%i == 0)
+               { an.push_back(i);
+                 N=N/i;
+               }
+        } 
+        
+        an.push_back(max(an[(an.size()-1)],N));
+        
+        
+       return *max_element(an.begin(), an.end());
+        
+       
+         
     }
 };
 
