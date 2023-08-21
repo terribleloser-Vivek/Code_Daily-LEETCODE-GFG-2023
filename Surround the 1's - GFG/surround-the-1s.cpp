c5771d5a8436ab3952573c95ca753db4924cfc58
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-08-21 23:31:42 +0530

    Added solution - LeetHub

---
+++ Surround the 1's - GFG/surround-the-1s.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int Count(vector<vector<int> >& matrix) {
+        int ans = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                int count= 0;
+                if(matrix[i][j] == 1)
+                {
+                    if(i!= 0 && matrix[i-1][j] == 0)  count++;
+                    if(i!=n-1 && matrix[i+1][j] == 0)  count++;
+                    if(j!=m-1 && matrix[i][j+1] == 0)  count++;
+                    if(j!= 0 && matrix[i][j-1]==0)  count++;
+                    if(i!=0 && j!=0 && matrix[i-1][j-1] == 0)  count++;
+                    if(i!=n-1 && j!=0 && matrix[i+1][j-1] == 0)  count++;
+                    if(i!=0 && j!=m-1 && matrix[i-1][j+1] == 0)  count++;
+                    if(i!=n-1 && j!=m-1 && matrix[i+1][j+1] == 0)  count++;
+                }
+                if(count!=0 && count%2 == 0)  ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>> matrix(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution ob;
+		int ans = ob.Count(matrix);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
